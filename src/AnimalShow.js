13d89826df2ed66f31dcf8c08a52e2e9105347e7
Gyu-Yeon <idsopy@nate.com>
2022-12-04 22:22:02 +0900

    3

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,33 @@
-function AnimalShow() {
+import { useState } from "react";
+import bird from "./svg//bird.svg";
+import cat from "./svg//cat.svg";
+import cow from "./svg//cow.svg";
+import dog from "./svg//dog.svg";
+import gator from "./svg//gator.svg";
+import heart from "./svg//heart.svg";
+import horse from "./svg//horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function AnimalShow(props) {
+  const [click, setClick] = useState(0);
+
+  const handleClick = () => {
+    setClick(click + 1);
+  };
+
+  const { type } = props;
   return (
-    <div>
-      <h1>cow</h1>
+    <div onClick={handleClick}>
+      <img src={svgMap[type]} alt="animals" />
+      <img src={heart} alt="heart" style={{ width: 10 + 10 * click + "px" }} />
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 import AnimalShow from "./AnimalShow";
 
+function getRandomAnimals() {
+  const animalList = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animalList[Math.floor(Math.random() * animalList.length)];
+}
+
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    const newAni = getRandomAnimals();
+    if (animals.includes(newAni) === false) {
+      setAnimals([...animals, newAni]);
+    } else return;
+  };
+
   return (
     <div className="App">
       <div>Show Animal list here!</div>
+      <button onClick={handleClick}>Add Animals</button>
+      <div>
+        {animals.map((type) => {
+          return <AnimalShow type={type} key={type} />;
+        })}
+      </div>
     </div>
   );
 }
